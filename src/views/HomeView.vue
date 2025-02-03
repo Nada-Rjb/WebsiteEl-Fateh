ef736fafb81dee79447c485519ff4afdb504117f
Nada-Rjb <enadaragab2020@gmail.com>
2025-02-03 02:56:02 +0200

    Add TopItems component to HomeView; display basic product cards with images

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,6 +2,7 @@
   <div class="home">
     <UpperBanner />
     <TheFeature />
+    <TopItems />
   </div>
 </template>
 
@@ -9,10 +10,12 @@
 // @ is an alias to /src
 import UpperBanner from "@/components/HomePage/UpperBanner.vue";
 import TheFeature from "@/components/HomePage/TheFeature.vue";
+import TopItems from "@/components/HomePage/TopItems.vue";
 export default {
   components: {
     UpperBanner,
     TheFeature,
+    TopItems,
   },
 };
 </script>
