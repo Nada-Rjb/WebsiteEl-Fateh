45e36ebf75dedecb48d0c3537b2d99981768331d
Nada-Rjb <enadaragab2020@gmail.com>
2025-02-05 00:18:13 +0200

    Add Swiper integration to FlashDeals component; replace layout with Swiper for improved product display

--- src/components/HomePage/FlashDeals.vue
+++ src/components/HomePage/FlashDeals.vue
@@ -1,28 +1,39 @@
 <template>
   <div class="flash-deals">
-    <v-container fluid>
-      <v-row>
-        <v-col cols="3" v-for="item in products" :key="item.id">
-          <v-card elevation="0" class="pl-0">
-            <img
-              :src="
-                item.thumbnail.startsWith('http')
-                  ? item.thumbnail
-                  : 'https://erp.elfateh.online' + item.thumbnail
-              "
-              class="w-100"
-              style="height: auto"
-              alt="flash sale"
-            />
-            <v-card-text dir="rtl">{{
-              item.item_name.split(" ").length < 3
-                ? item.item_name
-                : item.item_name.split(" ").slice(0, 9).join(" ") + "..."
-            }}</v-card-text>
-          </v-card>
-        </v-col>
-      </v-row>
-    </v-container>
+    <Swiper
+      :pagination="{ el: '.swiper-pagination', clickable: true }"
+      :modules="modules"
+      :slides-per-view="4"
+      :space-between="35"
+      :navigation="{
+        prevEl: '.swiper-button-prev',
+        nextEl: '.swiper-button-next',
+      }"
+    >
+      <SwiperSlide v-for="item in products" :key="item.id">
+        <v-card elevation="0" class="pl-0 bg-black" style="height: 300px">
+          <img
+            :src="
+              item.thumbnail.startsWith('http')
+                ? item.thumbnail
+                : 'https://erp.elfateh.online' + item.thumbnail
+            "
+            class="w-100"
+            height="200px"
+            alt="flash sale"
+          />
+          <v-card-text class="pl-0 pt0" dir="rtl">{{
+            item.item_name.split(" ").length < 3
+              ? item.item_name
+              : item.item_name.split(" ").slice(0, 9).join(" ") + "..."
+          }}</v-card-text>
+        </v-card>
+      </SwiperSlide>
+      <!-- to be at the and of page  -->
+      <div class="swiper-button-prev swiper-prev"></div>
+      <div class="swiper-button-next swiper-next"></div>
+      <div class="swiper-pagination"></div>
+    </Swiper>
   </div>
   <!-- {{ item.item_group }}
           Render product name 
@@ -30,22 +41,50 @@
   <!-- Render product price -->
 </template>
 <script>
+import { Swiper, SwiperSlide } from "vue-awesome-swiper";
+import { Pagination, Navigation } from "swiper";
 export default {
+  setup() {
+    return {
+      modules: [Navigation, Pagination],
+    };
+  },
   props: {
     products: {
       type: Array,
       required: true,
     },
   },
+  components: {
+    Swiper,
+    SwiperSlide,
+  },
   mounted() {
     console.log("this is flash deallls", this.products);
   },
 };
 </script>
-<style scoped>
-.v-card-text {
-  font-family: Cairo;
-  font-weight: 600;
-  font-size: 20px;
+<style scoped lang="scss">
+.flash-deals {
+  .v-card-text {
+    font-family: Cairo;
+    font-weight: 600;
+    font-size: 20px;
+  }
+  .swiper-button-next,
+  .swiper-button-prev {
+    background-color: rgb(116, 185, 116);
+    border-radius: 50%;
+    width: 35px;
+    height: 35px;
+    font-weight: 900;
+    color: white;
+    &::after {
+      font-size: 15px;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+    }
+  }
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -11,6 +11,12 @@ import { createVuetify } from "vuetify";
 import * as components from "vuetify/components";
 import * as directives from "vuetify/directives";
 import "@mdi/font/css/materialdesignicons.css";
+//swiper
+// In your main.js or entry point file
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
 
 const vuetify = createVuetify({
   components,
